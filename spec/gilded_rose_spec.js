fac0187070ccb2f911bbc556011a77b25e72f6bd
sandy <pounoussamy.sandy@gmail.com>
2021-04-09 17:47:22 +0100

    Added feature test for sulfuras product type

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -36,4 +36,21 @@ describe('#updateQuality', function() {
       })
     })
   })
+
+  describe('when the product type is sulfuras', function() {
+    it('the quality never goes down', function () {
+      const gildedRose = new Shop([ new Item("Sulfuras, Hand of Ragnaros", 0, 80) ]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toEqual(0);
+      expect(items[0].quality).toEqual(80);
+    })
+  })
+
+
+  it("the quality of an item is never negative", function() {
+    const gildedRose = new Shop([ new Item("Minor potion", -1, 0) ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toEqual(-2);
+    expect(items[0].quality).toEqual(0);
+  })
 });
